be3b56f9151e4f3cd606e341962f5a82c566a46f
dinger1986 <daniel.lamb@flonix.co.uk>
2022-08-18 00:16:11 +0100

    Create winget.ps1

---
+++ winget.ps1
@@ -0,0 +1,22 @@
+If(!(test-path C:\TEMP\TRMM))
+{
+      New-Item -ItemType Directory -Force -Path C:\TEMP\TRMM
+}
+
+cd c:\temp
+Invoke-WebRequest https://aka.ms/Microsoft.VCLibs.x64.14.00.Desktop.appx -OutFile vclibs.appx
+Invoke-WebRequest https://github.com/microsoft/winget-cli/releases/download/v1.4.2161-preview/Microsoft.DesktopAppInstaller_8wekyb3d8bbwe.msixbundle -Outfile winget.msixbundle
+Add-AppxPackage c:\temp\vclibs.appx
+Add-AppxPackage c:\temp\winget.msixbundle
+
+$wingetloc=(Get-Childitem -Path "C:\Program Files\WindowsApps" -Include winget.exe -Recurse -ErrorAction SilentlyContinue | %{$_.FullName} | Split-Path).lastone
+
+echo $wingetloc
+
+cd $wingetloc
+
+.\winget.exe upgrade --all --accept-source-agreements
+
+.\winget.exe" search packagename --accept-source-agreements
+
+.\winget.exe" install package.name --accept-source-agreements --accept-package-agreements
