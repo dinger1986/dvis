250797ee461a377044dba43998ef3668161260c9
dinger1986 <daniel.lamb@flonix.co.uk>
2023-06-03 11:10:28 +0100

    Update addswapfile.sh

--- addswapfile.sh
+++ addswapfile.sh
@@ -1,13 +1,5 @@
 #!/bin/bash
 
-### Ubuntu 20.04 Check
-
-UBU20=$(grep 20.04 "/etc/"*"release")
-if ! [[ $UBU20 ]]; then
-  echo -ne "\033[0;31mThis script will only work on Ubuntu 20.04\e[0m\n"
-  exit 1
-fi
-
 sudo fallocate -l 2G /swapfile
 sudo chmod 600 /swapfile
 sudo mkswap /swapfile
